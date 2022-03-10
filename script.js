f9da361d8b801ae0eb9e6e1c800898464e56f1e8
Maylie Vu <maylie.anhvu@gmail.com>
2022-03-10 11:00:48 -0500

    No changes

---
+++ script.js
@@ -0,0 +1,16 @@
+function change() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";}
+
+function reversechange() {
+    document.getElementById("demo").innerHTML = "This will change";}
+
+
+function print1() {
+    let x, y, z;
+    x=5; y=6; z=x+y;
+    document.getElementById("print1").innerHTML = "The value of z is: " + z + ".";
+}
+
+function showIMG() {
+    document.getElementById("image").src = "NusaPenidaINDO.jpg";
+}
